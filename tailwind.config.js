c43c8afef0d19abf71629a2ea89f84663b543ab2
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-05-21 19:45:18 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        dark1: "#111111",
+        dark2: "#414141",
+      },
+    },
+  },
+  plugins: [],
+};
+
+
+// value
+
+// String
+// Number
+// null
+// undefined
+// []
+// {}
+// ()
